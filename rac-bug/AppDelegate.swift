3a24034658fc6fa0f94bc1973c2e83a365d4835e
jeremiegirault <jeremie.girault@gmail.com>
2015-09-11 11:00:08 +0200

    initial commmit

---
+++ rac-bug/AppDelegate.swift
@@ -0,0 +1,81 @@
+//
+//  AppDelegate.swift
+//  rac-bug
+//
+//  Created by Jeremie Girault on 11/09/2015.
+//  Copyright (c) 2015 Jeremie Girault. All rights reserved.
+//
+
+import UIKit
+import ReactiveCocoa
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func signalProducers(#bug: Bool) {
+        let scheduler = QueueScheduler(priority: DISPATCH_QUEUE_PRIORITY_DEFAULT, name: "rac-bug.first")
+        
+        let signals = (0..<100).map { _ -> SignalProducer<Void, NoError> in
+            if bug {
+                return SignalProducer<Void, NoError>.empty |> startOn(scheduler)
+            }
+            return SignalProducer<Void, NoError>.empty
+        }
+        
+        combineLatest(signals) |> start()
+    }
+    
+    func signals() {
+        let signalPipe = Signal<Void, NoError>.pipe()
+        let signalProducer: SignalProducer<Void, NoError> = SignalProducer { (sink, disposable) in
+            let internalDisposable = signalPipe.0.observe(sink)
+            disposable.addDisposable(internalDisposable)
+        }
+        
+        signalProducer
+            |> on(started: {
+                sendCompleted(signalPipe.1)
+            })
+            |> on(event:{ e in println("event \(e)")} )
+            |> start()
+
+    }
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        signals()
+        signalProducers(bug: true)
+        
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
